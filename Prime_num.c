9afe56d4cc2fac0622299e2904a3c71581cbbd7a
Bilal-11 <sbilalahmad2000@gmail.com>
2021-08-12 09:18:51 +0530

    Added code to store numbers from 1 to 100 in an array.

--- Prime_num.c
+++ Prime_num.c
@@ -4,6 +4,18 @@
 
 int main()
 {
-  
+  int i, num[100];
+
+  /*Fill the array num with numbers from 1 to 100*/
+  for(i=0 ;i<100; i++)
+  {
+    num[i] = i + 1;
+  }
+
+  for(i=0 ;i<100; i++)
+  {
+    prinf("%d\n",num[i]);
+  }
+
   return 0;
 }
